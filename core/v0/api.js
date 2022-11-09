556115ff2cf1b239a9bf93a916bec9b102dfba4f
unknown <tolulope.adekunte@nicholasidoko.com>
2022-11-09 23:14:21 +0100

    test NIN

---
+++ core/v0/api.js
@@ -0,0 +1,66 @@
+function nin_check() {
+
+    var first_name = document.getElementById('first_name').value.toUpperCase();
+    var last_name = document.getElementById('last_name').value.toUpperCase();
+    var nin = document.getElementById('nin').value;
+
+    var all_data = {
+    "searchParameter": nin,
+    "verificationType":"NIN-SEARCH",
+    "transactionReference": ""
+}
+
+$.ajax({
+    headers: {
+        'userid': '1668011184920',
+        'apiKey': 'DTHswgJl1K3roPN5FeVp'
+      },
+    url: 'https://api.verified.africa/sfx-verify/v3/id-service/',
+    method: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(all_data),
+    contentType: 'application/json',
+    beforeSend: function(){
+        document.getElementById('show-error').style.display = 'none';
+        document.getElementById('sign-btn').style.display = 'none';
+        document.getElementById('sign-btn-load').style.display = 'block';
+    },
+    error: function(result){
+        if(result.description == 'Success') {
+
+            if(result.response[0].firstname == first_name && result.response[0].surname == last_name) {
+                window.location.href = "https://zigaara.netlify.app/success.html";
+            } else {
+                document.getElementById('error').innerHTML = 'Could not verify details';
+                document.getElementById('show-error').style.display = 'block';
+                document.getElementById('sign-btn').style.display = 'block';
+                document.getElementById('sign-btn-load').style.display = 'none';
+            }
+        } else {
+            document.getElementById('error').innerHTML = result.description;
+            document.getElementById('show-error').style.display = 'block';
+            document.getElementById('sign-btn').style.display = 'block';
+            document.getElementById('sign-btn-load').style.display = 'none';
+        } 
+    },
+    success: function(result){
+        if(result.description == 'Success') {
+
+            if(result.response[0].firstname == first_name && result.response[0].surname == last_name) {
+                window.location.href = "https://zigaara.netlify.app/success.html";
+            } else {
+                document.getElementById('error').innerHTML = 'Could not verify details';
+                document.getElementById('show-error').style.display = 'block';
+                document.getElementById('sign-btn').style.display = 'block';
+                document.getElementById('sign-btn-load').style.display = 'none';
+            }
+        } else {
+            document.getElementById('error').innerHTML = result.description;
+            document.getElementById('show-error').style.display = 'block';
+            document.getElementById('sign-btn').style.display = 'block';
+            document.getElementById('sign-btn-load').style.display = 'none';
+        } 
+    }
+});
+}
+
